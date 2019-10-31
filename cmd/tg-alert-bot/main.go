6223f4b02dc7c24583b7095970a01be6f3f0eb20
and07 <and_07@mail.ru>
2019-10-31 21:42:08 +0300

    test

--- cmd/tg-alert-bot/main.go
+++ cmd/tg-alert-bot/main.go
@@ -33,7 +33,7 @@ func sendMessageToBot(message string) {
 
 func main() {
 
-	ticker := time.NewTicker(1 * time.Hour)
+	ticker := time.NewTicker(6 * time.Hour)
 	for range ticker.C {
 		log.Println("Tick")
 		sendMessageToBot("test")
